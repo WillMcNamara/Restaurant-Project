bce2ff0e2d2ac0417e10a02fb85a0511e1efd6a1
Will M <wmcna97@gmail.com>
2019-04-30 14:29:03 -0400

    switching to master

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ let PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var tables = [];
+var waitlist = [];
 
+//gets for the 3 different pages
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
@@ -25,6 +28,16 @@ app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "table.html"));
 });
 
+//api information
+app.get("/api/tables", function(req, res){
+  res.json(tables);
+})
+
+app.get("/api/waitlist", function(req, res){
+  res.json(waitlist);
+})
+
+//listen function to make sure its working
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
